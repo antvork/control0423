22603b67a4c7f07d4760fccade91a0885dddc665
Anton Vorontsov <antvork@gmail.com>
2023-04-08 11:53:16 +0700

    added a soution via the method

--- task/Program.cs
+++ task/Program.cs
@@ -1,9 +1,23 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(string[]array)
+{
+    for(int i= 0; i<array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+}
 
 
-string[] myArray = { "apple", "oil", "a1", "table", "fruit", "l2", "2344" };
+string[] GetNewArray(string[]myArray)
+{
 string[] myNewArray = { };
 for (int i = 0, j = 0; i < myArray.Length; i++)
 {
@@ -14,5 +28,11 @@ for (int i = 0, j = 0; i < myArray.Length; i++)
         j++;
     }
 }
+return myNewArray;
+}
 
-Console.WriteLine(string.Join(", ", myNewArray));
+string[] text = new string[GetNumber("Введите длинну массива: ")];
+Console.WriteLine("Заполните массив: ");
+FillArray(text);
+string[]newText = GetNewArray(text);
+Console.WriteLine(string.Join(", ", newText));
